6a4e837ccef4bf7ac39104428054b1e8e6c0b2d1
Joshua <joshferns19@gmail.com>
2019-01-24 18:49:47 +0530

    Updates

--- events/urls.py
+++ events/urls.py
@@ -3,6 +3,6 @@ from django.views.generic import ListView, DetailView
 from events.models import Event
 
 urlpatterns = [ url(r'^$', ListView.as_view(queryset=Event.objects.all().order_by("-date")[:25],
-                                            template_name="events/events.html"))]
+                                            template_name="events.html"))]
                 #url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Event,
                 #                                         template_name = 'events/post.html'))]
